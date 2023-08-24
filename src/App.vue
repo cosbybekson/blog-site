c80abeeb5008e1604defd8c800f9404585414673
cosbybekson <bekoecosby.bc@gmail.com>
2023-08-24 15:52:24 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <home-page/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomePage from './views/HomePage.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {HomePage},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Manrope", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import bootstrap from "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/assets/css/main.css';
 
-createApp(App).mount('#app')
+
+createApp(App).use(bootstrap).mount('#app')
